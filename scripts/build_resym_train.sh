162720caa1bad3b09164a7d1ca8ce67733a6d767
lasserre <cstew33@gmail.com>
2024-12-31 21:35:24 -0600

    Added resym_train script, updated pip freeze versions

--- scripts/build_resym_train.sh
+++ scripts/build_resym_train.sh
@@ -1,7 +1,7 @@
 
 NJOBS=80
 DRAGON_DATA_FOLDER=~/dragon_data
-BIN_FOLDER=$DRAGON_DATA_FOLDER/tydamin_sample
+BIN_FOLDER=$DRAGON_DATA_FOLDER/resym_train
 
 ###########################################
 # run this from the top-level dragon folder
@@ -11,10 +11,10 @@ ORIG_DIR=`pwd`
 
 # create wildebeest experiment
 cd ./exps
-wdb create import-dataset tydamin_sample.exp -p bin_folder=$BIN_FOLDER
+wdb create import-dataset resym_trainbins.exp -p bin_folder=$BIN_FOLDER
 
 # run experiment (import into Ghidra, extract ASTs and AST var data)
-cd tydamin_sample.exp
+cd resym_trainbins.exp
 time wdb run -j$NJOBS
 
 cd $ORIG_DIR
